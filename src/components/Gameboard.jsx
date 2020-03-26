eb9658cf242be058093880029828cc1c1e18f761
Reuben Remy <reubenremy@gmail.com>
2020-03-26 14:08:01 -0400

    Completed PickCard Function

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -24,18 +24,31 @@ class Gameboard extends Component {
             deck: generateDeck(),
             pickedCards: []
         }
-    }
+    } 
 
     pickCard(cardIndex) {
-        if (!this.state.deck[cardIndex].isFlipped) {
-            return this.state.deck[cardIndex].setState({
-                isFlipped: true
-            });
-        } else {
-            return
-        }
+        //if the chosen card is already flipped we want to keep it the same so we'll return it which will essentially keep the card rendered as a front-facing card. 
+        if (this.state.deck[cardIndex].isFlipped) return;
 
+        //We're making a Copy essentially of whats happening in line 31. Why? because we dont want to edit it directly so we're making a copy and assigning it its own value that we can edit it and assign it something different later
+        const cardToFlip = {...this.state.deck[cardIndex]};
+        cardToFlip.isFlipped = true;
 
+        //Create a copy of the state array and adds chosen cards to it.
+        const newPickedCards = this.state.pickedCards.concat(cardIndex);
+
+        //Create a copy of the state of the new deck to copy
+        const newDeck = this.state.deck.map((card, index) => {
+            if (cardIndex === index) {
+                return cardToFlip;
+            }
+            return card;
+        })
+        
+        this.setState({
+            deck: newDeck,
+            pickedCards: newPickedCards
+        })
     }
 
     render() { 
@@ -46,6 +59,7 @@ class Gameboard extends Component {
                     key = { index }
                     isFlipped={ card.isFlipped }
                     symbol={ card.symbol }
+                    pickCard={ this.pickCard.bind(this, index) }
                 />
             )
         });
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,45 +1,28 @@
 import React, { Component } from 'react';
 import './MemoryCard.css';
 
-class MemoryCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isFlipped: false
-        };
-    }
-    
+function MemoryCard(props) { 
+    const { isFlipped } = props;
+    const memoryCardInnerClass = `MemoryCardInner ${ isFlipped === true && 'flipped'}`;
 
-    clickHandler() {
-        this.setState({
-            isFlipped: !this.state.isFlipped
-        })
-    };
-
-    render() { 
-        const { isFlipped } = this.props;
-
-        const memoryCardInnerClass = `MemoryCardInner ${ isFlipped === true && 'flipped'}`;
-
-        return ( 
-            <div 
-                className="MemoryCard"
-                onClick={() => this.clickHandler()}
-            >
-                <div className={ memoryCardInnerClass }>
-                    <div className="MemoryCardBack">
-                        <img 
-                            src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'
-                            alt='DigitalCrafts'
-                        />
-                    </div>
-                    <div className="MemoryCardFront">
-                        { this.props.symbol }
-                    </div>
+    return ( 
+        <div 
+            className="MemoryCard"
+            onClick={() => props.pickCard()}
+        >
+            <div className={ memoryCardInnerClass }>
+                <div className="MemoryCardBack">
+                    <img 
+                        src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'
+                        alt='DigitalCrafts'
+                    />
+                </div>
+                <div className="MemoryCardFront">
+                    { props.symbol }
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default MemoryCard;
