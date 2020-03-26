849723c9827aeae147a0a6409282f831a0d07bd7
Reuben Remy <reubenremy@gmail.com>
2020-03-25 22:13:14 -0400

    Small updates to card Component

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+    state = {
+        isFlipped: false
+    }
+
+    whenClicked = () => {
+        this.state ({
+            isFlipped: !this.state.isFlipped
+        })
+    };
+
     render() { 
+        const { isFlipped } = this.state;
+
         return ( 
-            <div className="MemoryCard">
+            <div className="MemoryCardBack">
                 <img 
                     src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'
                     alt='DigitalCrafts'
